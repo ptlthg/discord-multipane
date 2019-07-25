1e3b82036e1bb975fa963e3702b8a146f7dac7c4
Kaeso <edmier12@gmail.com>
2019-07-25 15:56:32 -0500

    Update

--- multipane.plugin.js
+++ multipane.plugin.js
@@ -64,7 +64,7 @@ const ChannelPopoutRemoveHTML = function removeHTML(){
     document.getElementsByName("ChannelPopout")[0].parentNode.remove();
 }
 
-var ChannelPopouts = (() {
+var ChannelPopouts = (() = {
     const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.4","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
 
     return !global.ZeresPluginLibrary ? class {
