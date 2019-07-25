a37b456af5e234772d6dfdf877718ff87f1e6aa1
Kaeso <edmier12@gmail.com>
2019-07-25 18:06:59 -0500

    Update

--- multipane.plugin.js
+++ multipane.plugin.js
@@ -1,4 +1,40 @@
 //META{"name":"Multipane","displayName":"Multipane","website":"https://github.com/ptlthg","source":"https://github.com/ptlthg/discord-multipane/blob/master/multipane.plugin.js"}*//
+class Multipane {
+    getName() {return "Multipane";}
+
+    getVersion() {return "1.0.0";}
+
+    getAuthor() {return "Kaeso";}
+
+    getDescription() {return "Enables multiple panes consistng of channels or DMs to be open at once";}
+
+    constructor() {
+        super();
+    }
+
+    load() {}
+
+    async start() {
+        //Logger.log("Started");
+        //Library.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
+        if(document.getElementsByName("Nova_Pin")[0] && !document.getElementsByName("ChannelPopout")[0]){
+            ChannelPopoutInjectHTML(document.getElementsByName("Nova_Pin")[0].parentNode);
+        }
+
+    }
+
+    stop() {
+        ChannelPopoutRemoveHTML();
+        //Logger.log("Stopped");
+    }
+
+    observer(e) {
+        if (e.addedNodes[0] && e.addedNodes[0].classList && e.addedNodes[0].getAttribute("name") === "Nova_Pin" && !document.getElementsByName("ChannelPopout")[0]){
+            let wrapper = document.createElement('div');
+            ChannelPopoutInjectHTML(e.addedNodes[0].parentNode);
+        }
+    }
+};
 
 function ChannelPopoutOnMouseEnter(){
     let wrapper = document.createElement('div');
@@ -63,94 +99,3 @@ const ChannelPopoutInjectHTML = function injectHTML(icon){
 const ChannelPopoutRemoveHTML = function removeHTML(){
     document.getElementsByName("ChannelPopout")[0].parentNode.remove();
 }
-
-var Multipane = (() => {
-  const config = {
-    "info":{
-      "name":"Multipane",
-      "authors":[{
-        "name":"Kaeso",
-        "discord_id":"174265140357627904",
-        "github_username":"ptlthg"
-      }],
-      "version":"1.0.0",
-      "description":"Enables multiple panes consistng of channels or DMs to be open at once",
-      "github":"",
-      "github_raw":"https://raw.githubusercontent.com/ptlthg/discord-multipane/master/multipane.plugin.js"},
-      "main":"index.js"};
-
-      return class Multipane {
-          getName() {return "Multipane";}
-          getVersion() {return "1.0.0";}
-          getAuthor() {return "Kaeso";}
-          getDescription() {return "Enables multiple panes consistng of channels or DMs to be open at once";}
-          
-          constructor() {
-              super();
-          }
-
-          onStart() {
-              //Logger.log("Started");
-              //Library.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
-              if(document.getElementsByName("Nova_Pin")[0] && !document.getElementsByName("ChannelPopout")[0]){
-                  ChannelPopoutInjectHTML(document.getElementsByName("Nova_Pin")[0].parentNode);
-              }
-
-          }
-
-          onStop() {
-              ChannelPopoutRemoveHTML();
-              //Logger.log("Stopped");
-          }
-
-          observer(e){
-              if(e.addedNodes[0] && e.addedNodes[0].classList && e.addedNodes[0].getAttribute("name") === "Nova_Pin" && !document.getElementsByName("ChannelPopout")[0]){
-                  let wrapper = document.createElement('div');
-                  ChannelPopoutInjectHTML(e.addedNodes[0].parentNode);
-              }
-          }
-      };
-})();
-/*var ChannelPopouts = (() => {
-    const config = {"info":{"name":"ChannelPopouts","authors":[{"name":"Green","discord_id":"80593258903773184","github_username":"Curtis-D"}],"version":"1.1.4","description":"Allows you to popout DMs/Servers to view more than one DM/Server at a time.","github":"","github_raw":"https://raw.githubusercontent.com/Curtis-D/ChannelPopouts/master/ChannelPopouts.plugin.js"},"main":"index.js"};
-
-    return !global.ZeresPluginLibrary ? class {
-        getName() {return config.info.name;} getAuthor() {return config.info.authors.map(a => a.name).join(", ");} getDescription() {return config.info.description;} getVersion() {return config.info.version;}
-        showAlert() {window.mainCore.alert("Library Missing",`The Library needed for this plugin is missing, please download it from here: <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/ZeresPluginLibrary/0PluginLibrary.plugin.js">https://github.com/rauenzi/BetterDiscordAddons/tree/master/Plugins/ZeresPluginLibrary</a>`);} load() {this.showAlert();} start() {this.showAlert();} stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
-
-    //const {Logger, Patcher, Settings} = Library;
-
-
-    return class ChannelPopouts extends Plugin {
-        constructor() {
-            super();
-        }
-
-        onStart() {
-            //Logger.log("Started");
-            //Library.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.github_raw);
-            if(document.getElementsByName("Nova_Pin")[0] && !document.getElementsByName("ChannelPopout")[0]){
-                ChannelPopoutInjectHTML(document.getElementsByName("Nova_Pin")[0].parentNode);
-            }
-
-        }
-
-        onStop() {
-            ChannelPopoutRemoveHTML();
-            //Logger.log("Stopped");
-        }
-
-        observer(e){
-            if(e.addedNodes[0] && e.addedNodes[0].classList && e.addedNodes[0].getAttribute("name") === "Nova_Pin" && !document.getElementsByName("ChannelPopout")[0]){
-                let wrapper = document.createElement('div');
-                ChannelPopoutInjectHTML(e.addedNodes[0].parentNode);
-            }
-        }
-    };
-
-};
-        //return plugin(Plugin, Api);
-    })(/*global.ZeresPluginLibrary.buildPlugin(config));
-})();*/
