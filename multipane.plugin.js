b7d148423c766c74ef2de1fb286a11912e5eae44
Kaeso <edmier12@gmail.com>
2019-07-26 21:38:08 -0500

    New Icon

--- multipane.plugin.js
+++ multipane.plugin.js
@@ -1,18 +1,18 @@
 //META{"name":"Multipane"}*//
 
-function MultiPaneOnMouseEnter(){
+function MultiPaneOnMouseEnter() {
     let wrapper = document.createElement('div');
-    let buttonLeft = parseInt(document.getElementsByName('MultiPane')[0].getBoundingClientRect().left) - 36;
+    let buttonLeft = parseInt(document.getElementsByName('MultiPane')[0].getBoundingClientRect().left) - 25;
     let buttonTop = parseInt(document.getElementsByName('MultiPane')[0].getBoundingClientRect().top) + 25;
-    wrapper.innerHTML = `<div class='layer-v9HyYc da-layer MultiPaneIcon' style='left: ` + buttonLeft.toString() + `px; top: ` + buttonTop.toString() + `px;'><div class="tooltip-2QfLtc tooltipBottom-3ARrEK tooltipBlack-PPG47z"><div class="tooltipPointer-3ZfirK da-tooltipPointer"></div>Seperate</div></div>`;
+    wrapper.innerHTML = `<div class='layer-v9HyYc da-layer MultiPaneIcon' style='left: ` + buttonLeft.toString() + `px; top: ` + buttonTop.toString() + `px;'><div class="tooltip-2QfLtc tooltipBottom-3ARrEK tooltipBlack-PPG47z"><div class="tooltipPointer-3ZfirK da-tooltipPointer"></div>Separate</div></div>`;
     document.querySelector('.layerContainer-yqaFcK').appendChild(wrapper.firstChild);
 };
 
-function MultiPaneOnMouseLeave(){
+function MultiPaneOnMouseLeave() {
     document.querySelector('.MultiPaneIcon').remove();
 };
 
-function MultiPaneOnMouseClick(){
+function MultiPaneOnMouseClick() {
     console.log('Woo')
 };
 
@@ -21,20 +21,19 @@ const MultiPaneInjectHTML = function injectHTML(icon){
 
     if(icon && !document.getElementsByName("MultiPane")[0]){
         wrapper.innerHTML = `<div tabindex="0" class="iconWrapper-2OrFZ1 da-iconWrapper clickable-3rdHwn da-clickable" role="button">
-            <svg class="icon-22AiRD da-icon" name="MultiPane" width="16" height="16" viewBox="-8 -8 80 80" fill = "currentColor">
-                <g>
-                    <g>
-                        <g>
-                            <polygon points="30,56 8,56 8,11 30,11 25,3 0,3 0,64 25,64"/>
-                        </g>
-                    </g>
-                    <g>
-                        <g>
-                            <polygon points="34,19 26,19 26,29.5 15.5,29.5 15.5,37.5 26,37.5 26,48 34,48 34,37.5 44.5,37.5 44.5,29.5 34,29.5"/>
-                        </g>
-                    </g>
-                </g>
-            </svg>
+          <svg class="icon-22AiRD da-icon" name="MultiPane" width="16" height="16" viewBox="-8 -8 80 80" fill="currentColor">
+            <g>
+              <g>
+                <rect x="0" y="0" rx="5" ry="5" width="40" height="64" stroke="black" style="stroke-width:1;stroke-opacity:0.5;"/>
+              </g>
+              <g>
+                <rect x="44" y="8" rx="0" ry="0" width="12" height="48" />
+              </g>
+              <g>
+                <rect x="44" y="8" rx="3" ry="3" width="20" height="48" />
+              </g>
+            </g>
+          </svg>
         </div>`;
         icon.parentNode.prepend(wrapper.firstChild);
         document.getElementsByName("MultiPane")[0].onmouseenter = MultiPaneOnMouseEnter;
